e24ed82ddcbdf4795a49c7934506b8c047ec2c63
ywelement <yw@discoverelement.com>
2015-09-01 15:43:59 -0400

    start adding export in experiment

--- Experiment.lua
+++ Experiment.lua
@@ -71,6 +71,26 @@ function Xp:hyperParam(name, value, update)
    end
 end
 
+-- fetch all the hyper parameter names from db
+function Xp:fetchHyperParamNames()
+   assert(torch.type(name) == 'string')
+   local rows, err = self.conn:fetch([[
+   SELECT distinct param_name FROM %s.param 
+   WHERE hex_id = %s
+   ]], {self.conn.schema, self.id})
+
+   Xp:assert(torch.type(rows) == 'table')
+   --Xp:assert(#rows == 2, "Postgres select serialize err")
+   --Xp:assert(#rows[1] == 0, "Postgres missing columns err")
+
+   if rows then
+      -- rows = _.slice(rows, 3)
+      return rows, err
+   else
+      return nil, err
+   end
+end
+
 -- meta-data get/set
 -- Unlike hyper-params, metadata do/should not influence the results of the experiment.
 function Xp:metaData(name, value, update)
